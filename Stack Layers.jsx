b373de982352039e054332bc8e68c6923c92934b
Andrew S Klug // ASK <52141120+studioASK@users.noreply.github.com>
2022-04-16 12:21:09 -0700

    Delete Stack Layers.jsx

--- Stack Layers.jsx
+++
@@ -1,8 +0,0 @@
-// studioASK
-var askDoc1 = app.documents.add(2160, 2160);
-for(var counter = 1; counter < (300+1); counter++) {
-  var askDoc2 = app.open(File("~/ASKproduKtion Dropbox/Andrew Klug/studioASK/images/" + counter + ".png"));
-  askDoc2.artLayers["Layer 1"].copy();
-  askDoc2.close();
-  askDoc1.paste();
-};
