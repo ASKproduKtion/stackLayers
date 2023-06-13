7dc9e58a1becb4b3b1e0a30df7c6629a415760a4
Andrew S Klug // ASK <52141120+ASKproduKtion@users.noreply.github.com>
2023-06-13 09:07:23 -0700

    Update stack layers.jsx

--- stack layers.jsx
+++ stack layers.jsx
@@ -1,8 +1,9 @@
-// studioASK
-const numFrames = 600;
+// ASKproduKtion.com
+
+const numFrames = 48;
 var askDoc1 = app.documents.add(2160, 2160);
 for(var counter = 0; counter < numFrames; counter++) {
-  var fileRef = File("~/ASKproduKtion Dropbox/Andrew Klug/studioASK/images/" + (counter+1) + ".png")
+  var fileRef = File("~/ASKproduKtion Dropbox/Andrew Klug/scripting ops/_images/" + (counter+1) + ".png")
   var askDoc2 = app.open(fileRef);
   askDoc2.artLayers["Layer 1"].copy();
   askDoc2.close();
